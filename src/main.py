0e29732120accc6e9b345642e7e592f560b2ea85
Nikita <grishchenkonikita@mail.ru>
2023-10-12 10:05:56 +0300

    feat: cors

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.operation.router import router as operation_router
 
@@ -10,12 +11,24 @@ from src.user.schemas import UserCreate, UserRead, UserUpdate
 
 app = FastAPI(title="ispent")
 
+origins = [
+    "http://localhost:9000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_router, prefix="/user", tags=["user"])
 app.include_router(operation_router, prefix="/operation", tags=["operation"])
 
 app.include_router(
-    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth", tags=["auth"]
 )
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
