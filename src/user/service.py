e6fd1183b3668568ad47185e0e1e057ba03f5543
Nikita <grishchenkonikita@mail.ru>
2023-10-17 22:01:08 +0300

    calc total sum user operations

--- src/user/service.py
+++ src/user/service.py
@@ -69,21 +69,34 @@ async def create_category_user(
     return new_category_user
 
 
-def calc_difference_user_operations(operations):
-    total_balance = monthly_income = monthly_expenses = 0
+def calc_total_user_operations(operations):
+    result = 0
+    for operation in operations:
+        if operation.kind == 1:
+            result += operation.amount
+        else:
+            result -= operation.amount
+    return result
 
-    return total_balance, monthly_income, monthly_expenses
+
+def calc_difference_user_operations(operations):
+    total_income = total_expenses = 0
+    for operation in operations:
+        if operation.kind == 1:
+            total_income += operation.amount
+        else:
+            total_expenses -= operation.amount
+    return total_income, total_expenses
 
 
 async def get_overview_data_user(session: AsyncSession, user_id: int):
     user_operations = await get_operations(session, user_id)
-    total_balance, monthly_income, monthly_expenses = calc_difference_user_operations(
-        user_operations
-    )
+    total_balance = calc_total_user_operations(user_operations)
+    total_income, total_expenses = calc_difference_user_operations(user_operations)
     # categories_user = await session.execute(
     #     select(models.CategoryUser).where(models.CategoryUser.user_id == user_id)
     # )
-    return total_balance, monthly_income, monthly_expenses
+    return total_balance, total_income, total_expenses
 
 
 # def get_user_by_email(db: Session, email: str):
